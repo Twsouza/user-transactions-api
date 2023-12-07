b32726e6b6c062654ec25645f364303b70c853db
Taynan Souza <taynan.tws@gmail.com>
2023-12-07 13:29:45 -0300

    Add connection pool settings to database.go
    Set max idle connection to 20
    And MaxLifetime to 1 hour
    Max open connections to 100 (pgsql default)

--- application/data/database.go
+++ application/data/database.go
@@ -49,5 +49,16 @@ func (d *Database) Connect() (*gorm.DB, error) {
 		d.Db.AutoMigrate(core.Transaction{})
 	}
 
+	sqlDB, _ := d.Db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(20)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	return d.Db, nil
 }
